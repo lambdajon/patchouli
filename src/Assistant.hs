7bd7eebf33cb3ba50fc4a0fc96772a758ccf0974
thelissimus <thelissimus@tuta.io>
2023-11-27 08:25:27 +0500

    add: initial Assistant algebra

--- src/Assistant.hs
+++ src/Assistant.hs
@@ -1,1 +1,19 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Assistant () where
+
+import Data.Kind (Type)
+import Effectful (Dispatch (Dynamic), DispatchOf, Effect)
+import Effectful.TH (makeEffect)
+
+type Assistant :: (Type -> Type) -> Type -> Type
+data Assistant :: Effect where
+  ListResources :: Assistant m ()
+  SearchBySignature :: String -> Assistant m ()
+  SearchPackageByName :: String -> Assistant m ()
+
+type instance DispatchOf Assistant = 'Dynamic
+
+makeEffect ''Assistant
